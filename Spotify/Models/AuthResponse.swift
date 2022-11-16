8a581de9946ce7cfb1e60075376c39365093afc5
32tarcan <bahadir.tarcan41@gmail.com>
2022-11-16 22:32:46 +0300

    Auth codable update.

--- Spotify/Models/AuthResponse.swift
+++ Spotify/Models/AuthResponse.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+struct AuthResponse: Codable {
+    
+    let access_token:String
+    let expires_in:Int
+    let refresh_token:String?
+    let scope:String
+    let token_type:String
+    
+}
+
